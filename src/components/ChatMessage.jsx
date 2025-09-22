19b3bbe296da054647f2805689822cdf330de99b
Jeremy Dosdo Purba <jeremydosdo@gmail.com>
2025-09-22 14:36:00 +0700

    add profile picture for user

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,8 +1,9 @@
 import RobotProfileImage from "../assets/robot.png";
-import UserProfileImage from "../assets/user.png";
+import UserProfileImage from "../assets/profile-1.jpg";
 import "./ChatMessage.css"
 
 export function ChatMessage({ message, sender }) {
+  console.log(UserProfileImage)
   // const message = props.message
   // const sender = props.sender
   // const { message, sender } = props
